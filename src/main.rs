4a15f9bd94db1c7e7b4ccf74bbfbead5f8fc6167
Troy Marshall <troymarshall1966@gmail.com>
2024-10-20 20:22:19 -0400

    Add color to messages

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use colored::{self, Colorize};
 
 fn main() {
     println!("Guess the number from 1 to 100!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -26,10 +27,10 @@ fn main() {
         println!("You guessed {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
             Ordering::Equal => {
-                println!("You win!");
+                println!("{}", "You win!".green());
                 break;
             }
         }
